59e7a255f879a6d78e1c81cb37222c70cd14c70d
Tim Cornwell <t.cornwell@skatelescope.org>
2015-02-20 08:58:26 +0000

    Optimisation of circles

--- lowopt.py
+++ lowopt.py
@@ -3,30 +3,32 @@ from telopt import *
 #
 # Radius=1/2 FWZ ~ FWHM
 #
+random.seed(781490893)
 ts=TelSources()
-ts.construct(nsources=1, radius=6.0/35.0)
+ts.construct(nsources=5, radius=6.0/35.0)
 
-rshake=20.0
+rshake=5.0
 
 sbest=0
 lowbest=TelArray()
 
 lowrand=TelArray()
-lowrand.construct('LOW_OPTIMUM', nstations=52, nhalo=52, rhalo=40)
-lowbest=lowrand
+lowrand.construct('LOW_OPTIMUM', nstations=60, nhalo=60, rhalo=40)
+lowfirst=lowrand
 
 tp=TelPiercings()
 tp.construct(ts,lowrand,rmin=1.0,hiono=300)
 sbest = tp.assess(nnoll=50, rmax=50)
-print "Initial s=%f" % sbest
+print "Initial shalf=%f" % sbest
 
 for trial in range(100):
+	random.seed(781490893)
 
-	lowrand=lowbest
+	lowrand=lowfirst
 	lowrand.shakehalo(rshake)
 
 	tp=TelPiercings()
-	tp.construct(ts,lowrand,rmin=2.5,hiono=300)
+	tp.construct(ts,lowrand,rmin=1.0,hiono=300)
 	shalf = tp.assess(nnoll=50, rmax=50)
 	tp=False
 	
@@ -34,7 +36,6 @@ for trial in range(100):
 		print "Trial %d - New best array : shalf=%f, rshake=%f" % (trial, shalf, rshake)
 		sbest=shalf
 		lowbest=lowrand
-#		rshake=rshake*0.95
 		lowbest.plot()
 	else:
 		print "Trial %d - failed : shalf=%f" % (trial, shalf)
