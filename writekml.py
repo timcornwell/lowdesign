c07b73500a07b5dbc2fcfd601c7cca2407bf34ce
Tim Cornwell <t.cornwell@skatelescope.org>
2015-04-06 10:31:11 +0100

    Best Boolardy arrays
    As sent to Cath Trott

--- writekml.py
+++ writekml.py
@@ -1,5 +1,7 @@
 from telopt import *
 
-lowopt=TelArray()
-lowopt.readLOWBD('LOW_RANDOMBOOLARDY46', l1def='LOW_RANDOMBOOLARDY46.csv')
-lowopt.writeKML('LOW_RANDOMBOOLARDY46.kml')
+for file in ['LOW_RANDOMBOOLARDY11', 'LOW_RANDOMBOOLARDY31', 'LOW_RANDOMBOOLARDY46', 'LOW_RANDOMBOOLARDY21', 'LOW_RANDOMBOOLARDY41', 'LOW_RANDOMBOOLARDY51']:
+	lowopt=TelArray()
+	print file
+	lowopt.readLOWBD('file', l1def='%s.csv' % file)
+	lowopt.writeKML('%s.kml' % file)
